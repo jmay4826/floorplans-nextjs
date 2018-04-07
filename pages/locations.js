e5d038f8ba7f5811f30ef3e41957b07da5b22a26
Jonathan May <jmay4826@gmail.com>
2018-04-06 23:12:23 -0400

    starting to work on login

--- pages/locations.js
+++ pages/locations.js
@@ -1,12 +1,13 @@
-import TextField from "material-ui/TextField";
-import Card, { CardText, CardTitle } from "material-ui/Card";
+import TextField from 'material-ui/TextField';
+import Card, { CardText, CardTitle } from 'material-ui/Card';
+import axios from 'axios';
 
-import withMui from "../lib/withMui";
-import withData from "../lib/withData";
-import Layout from "../components/Layout";
-import FlatButton from "material-ui/FlatButton";
+import withMui from '../lib/withMui';
+import withData from '../lib/withData';
+import Layout from '../components/Layout';
+import FlatButton from 'material-ui/FlatButton';
 
-import LocationList from "../components/LocationList";
+import LocationList from '../components/LocationList';
 
 class Locations extends React.Component {
   render() {
--- pages/login.js
+++ pages/login.js
@@ -1,29 +1,35 @@
-import axios from "axios";
-import Router from "next/router";
-import Link from "next/link";
+import React from 'react';
+import axios from 'axios';
+import Router from 'next/router';
+import Link from 'next/link';
 
-import TextField from "material-ui/TextField";
-import Card, { CardText } from "material-ui/Card";
+import TextField from 'material-ui/TextField';
+import Card, { CardText } from 'material-ui/Card';
 
-import withMui from "../lib/withMui";
-import Layout from "../components/Layout";
-import FlatButton from "material-ui/FlatButton";
+import withMui from '../lib/withMui';
+import Layout from '../components/Layout';
+import FlatButton from 'material-ui/FlatButton';
 
 class Index extends React.Component {
-  static async getInitialProps(free) {
-    // const req = await axios("http://localhost:5000/api/users");
-    // const users = await req.json();
-    // return { users };
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: ''
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   handleSubmit() {
-    // axios
-    //   .post("http://localhost:5000/auth/login", {
-    //     username: "demoadmin",
-    //     password: "password"
-    //   })
-    //   .then(console.log)
-    //   .catch(console.log);
+    console.log('submitting');
+    axios
+      .post('http://localhost:5000/auth/login', {
+        username: this.state.username,
+        password: this.state.password
+      })
+      .then(console.log)
+      .catch(console.log);
+    Router.push('/locations');
   }
 
   render() {
@@ -33,11 +39,17 @@ class Index extends React.Component {
           <Card>
             <CardText>
               <div>
-                <TextField hintText="Username" />
-                <TextField hintText="Password" />
-                <Link href="/locations">
-                  <FlatButton onClick={this.handleSubmit}>Submit</FlatButton>
-                </Link>
+                <TextField
+                  hintText="Username"
+                  onChange={e => this.setState({ username: e.target.value })}
+                />
+                <TextField
+                  type="Password"
+                  hintText="Password"
+                  onChange={e => this.setState({ password: e.target.value })}
+                />
+
+                <FlatButton onClick={this.handleSubmit}>Submit</FlatButton>
               </div>
             </CardText>
           </Card>
